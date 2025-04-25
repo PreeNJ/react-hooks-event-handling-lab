d4d670ff1c2a1198bb8ff92964d5b164c13f5f34
Priscillah Njai <priscillahnjaii@gmail.com>
2025-04-25 09:23:25 +0300

    Final commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  const handleFocus = () => {
+    console.log("Good!");
+  };
+
+  const handleBlur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  return (
+    <div className="eyes-on-me">
+      <button 
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      >
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,21 @@
 // Code Keypad Component Here
+import React, { useState } from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handleChange = (event) => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <div className="keypad">
+      <h2>Security Keypad</h2>
+      <input 
+        type="password"
+        onChange={handleChange}
+        placeholder="Enter password" 
+      />
+    </div>
+  );
 }
 
 export default Keypad;
